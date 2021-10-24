38edf657ccbcf2280d111a80a3f7f7b7c623e882
Kevin McPeak <kevmc46@gmail.com>
2021-10-24 11:19:14 -0400

    added reset functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let main = (name) =>{
     let toggle = true
     let board = document.getElementsByClassName("gameboard")[0];
+    let children = board.children;  
+    let arr = Array.from(children) 
     let gameBoard = 
     [" ", " "," ",
     " ", " "," ",
@@ -14,6 +16,22 @@ let main = (name) =>{
     let Kevin = player("Kevin")
     let Erin = player("Erin");
 
+    let resetGame = (gboard, userboard) =>{
+            gboard[0] = ' ';
+            gboard[1] = ' ';
+            gboard[2] = ' ';
+            gboard[3] = ' ';
+            gboard[4] = ' ';
+            gboard[5] = ' ';
+            gboard[6] = ' ';
+            gboard[7] = ' ';
+            gboard[8] = ' ';
+            gboard[9] = ' ';
+
+            userboard.forEach(function(el){
+                el.innerHTML = ' ';
+            })
+          }
     
 
     let checkWinner = () => {
@@ -25,7 +43,9 @@ let main = (name) =>{
             gameBoard[2] === "X" && gameBoard[4] === "X" && gameBoard[6] === "X" ||
             gameBoard[0] === "X" && gameBoard[4] === "X" && gameBoard[8] === "X" ||
             gameBoard[2] === "X" && gameBoard[5] === "X" && gameBoard[8] === "X"){
-            console.log(`${Kevin.name} wins!`)} else if(
+            console.log(`${Kevin.name} wins!`)
+            resetGame(gameBoard, arr)}
+            else if(
             gameBoard[0] === "O" && gameBoard[1] === "O" && gameBoard[2] === "O" ||
             gameBoard[3] === "O" && gameBoard[4] === "O" && gameBoard[5] === "O" ||
             gameBoard[6] === "O" && gameBoard[7] === "O" && gameBoard[8] === "O" ||
@@ -34,10 +54,13 @@ let main = (name) =>{
             gameBoard[2] === "O" && gameBoard[5] === "O" && gameBoard[8] === "O" ||
             gameBoard[0] === "O" && gameBoard[4] === "O" && gameBoard[8] === "O" ||
             gameBoard[2] === "O" && gameBoard[4] === "O" && gameBoard[6] === "O"){
-                console.log(`${Erin.name} wins!`)} else if(
-                gameBoard.includes(" ") === false){console.log("tie game!")}
-        
+            console.log(`${Erin.name} wins!`)
+            resetGame(gameBoard, arr)}
+            else if(
+            gameBoard.includes(" ") === false){console.log("tie game!")
+            resetGame(gameBoard, arr)}
         }
+
     let marker = (toggle)=>{
         if(toggle === true){
             return 'X'
@@ -48,8 +71,7 @@ let main = (name) =>{
       
 
     let clickBoard = () =>{
-        let children = board.children;  
-        let arr = Array.from(children)  
+         
         
         arr.forEach(function(el){
             el.addEventListener("click", () => {
